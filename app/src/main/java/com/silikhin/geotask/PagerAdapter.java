3eb1a3924f5aa8e4eae45aff39de97e62ce007b4
Borys Silikhin <silihin@gmail.com>
2017-04-17 18:14:55 +0300

    added tabs and pager adapter for MainActivity

---
+++ app/src/main/java/com/silikhin/geotask/PagerAdapter.java
@@ -0,0 +1,39 @@
+package com.silikhin.geotask;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+/**
+ * Created by B_Silikhin on 017 17.04.17.
+ */
+
+public class PagerAdapter extends FragmentStatePagerAdapter {
+    int mNumOfTabs;
+
+    public PagerAdapter(FragmentManager fm, int NumOfTabs) {
+        super(fm);
+        this.mNumOfTabs = NumOfTabs;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+
+        switch (position) {
+            case 0:
+                TabFragmentFrom tab1 = new TabFragmentFrom();
+                return tab1;
+            case 1:
+                TabFragmentTo tab2 = new TabFragmentTo();
+                return tab2;
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return mNumOfTabs;
+    }
+}
+
