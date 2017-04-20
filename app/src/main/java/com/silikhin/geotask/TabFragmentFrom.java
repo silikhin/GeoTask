fed3ee4d06a175631c7037299994ba0bd87c1bc9
Borys Silikhin <silihin@gmail.com>
2017-04-20 09:46:32 +0300

    map fragment added into TabFragment

--- app/src/main/java/com/silikhin/geotask/TabFragmentFrom.java
+++ app/src/main/java/com/silikhin/geotask/TabFragmentFrom.java
@@ -12,13 +12,17 @@ import com.google.android.gms.common.api.Status;
 import com.google.android.gms.location.places.Place;
 import com.google.android.gms.location.places.ui.PlaceSelectionListener;
 import com.google.android.gms.location.places.ui.SupportPlaceAutocompleteFragment;
-
+import com.google.android.gms.maps.*;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class TabFragmentFrom extends Fragment {
+public class TabFragmentFrom extends Fragment implements OnMapReadyCallback{
+
+    private GoogleMap mMap;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -26,16 +30,27 @@ public class TabFragmentFrom extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.tab_fragment_from, container, false);
 
+        SupportMapFragment mapFragment = new SupportMapFragment();
         SupportPlaceAutocompleteFragment autocompleteFragment = new SupportPlaceAutocompleteFragment();
         getActivity().getSupportFragmentManager()
                 .beginTransaction()
                 .replace(R.id.place_autocomplete_from, autocompleteFragment)
+                .replace(R.id.mapFrom, mapFragment)
                 .commit();
+
+        mapFragment.getMapAsync(this);
 
         autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
             @Override
             public void onPlaceSelected(Place place) {
-                Log.i("myLogs", "Place: " + place.getName());
+                if (mMap!=null){
+                    mMap.clear();
+
+                    LatLng choosenPlaceLatLng = place.getLatLng();
+                    String choosenPlaceTitle = place.getName().toString();
+                    mMap.addMarker(new MarkerOptions().position(choosenPlaceLatLng).title(choosenPlaceTitle));
+                    mMap.moveCamera(CameraUpdateFactory.newLatLng(choosenPlaceLatLng));
+                }
             }
 
             @Override
@@ -47,4 +62,8 @@ public class TabFragmentFrom extends Fragment {
         return view;
     }
 
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+    }
 }
