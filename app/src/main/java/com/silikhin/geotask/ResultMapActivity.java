8cc7aacc130b065c5d40c8c45b24b7e26e567fa9
Borys Silikhin <silihin@gmail.com>
2017-04-25 10:10:20 +0300

    fixed bugs, Polyline draws corectly

--- app/src/main/java/com/silikhin/geotask/ResultMapActivity.java
+++ app/src/main/java/com/silikhin/geotask/ResultMapActivity.java
@@ -25,6 +25,7 @@ import com.google.android.gms.maps.UiSettings;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.Polyline;
 import com.google.android.gms.maps.model.PolylineOptions;
 import com.google.gson.Gson;
@@ -48,17 +49,19 @@ public class ResultMapActivity extends AppCompatActivity implements OnMapReadyCa
     private GoogleApiClient mGoogleApiClient;
     private GoogleMap mMap;
     private CameraPosition mCameraPosition;
-    private final String TAG = "myLogs";
-    private LatLng mLatLngFrom, mLatLngTo, mLatLngLastKnown;
-    private String stringLatLngFrom, stringLatLngTo;
+
+    private boolean mLocationPermissionGranted;
+    private static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1;
 
     private Location mLastKnownLocation;
+    private LatLng mLatLngFrom, mLatLngTo, mLatLngLastKnown;
+    private String mStringLatLngFrom, mStringLatLngTo;
 
-    private StartLocation routeStartLocation;
-    private ArrayList<EndLocation> routePoints = new ArrayList<>();
+    private StartLocation mRouteStartLocation;
+    private ArrayList<EndLocation> mRoutePoints = new ArrayList<>();
 
-    private static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1;
-    private boolean mLocationPermissionGranted;
+    private final String TAG = "myLogs";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -69,10 +72,8 @@ public class ResultMapActivity extends AppCompatActivity implements OnMapReadyCa
 
         mLatLngFrom = getIntent().getBundleExtra("bundle").getParcelable("latLngFrom");
         mLatLngTo = getIntent().getBundleExtra("bundle").getParcelable("latLngTo");
-        stringLatLngFrom = mLatLngFrom.latitude + "," + mLatLngFrom.longitude;
-        stringLatLngTo = mLatLngTo.latitude + "," + mLatLngTo.longitude;
-        Log.d(TAG, "Loc from = " + mLatLngFrom);
-        Log.d(TAG, "Loc to = " + mLatLngTo);
+        mStringLatLngFrom = mLatLngFrom.latitude + "," + mLatLngFrom.longitude;
+        mStringLatLngTo = mLatLngTo.latitude + "," + mLatLngTo.longitude;
 
         mGoogleApiClient = new GoogleApiClient.Builder(this)
                 .enableAutoManage(this, this)
@@ -92,32 +93,72 @@ public class ResultMapActivity extends AppCompatActivity implements OnMapReadyCa
         RouteAPI routeAPI = retrofit.create(RouteAPI.class);
 
         Call<RouteApiResponse> call = routeAPI.getRoute(
-                stringLatLngFrom,
-                stringLatLngTo,
+                mStringLatLngFrom,
+                mStringLatLngTo,
                 getResources().getString(R.string.google_maps_key)
         );
-        Log.d(TAG, call.request().toString());
         call.enqueue(this);
     }
 
     @Override
     public void onConnected(@Nullable Bundle bundle) {
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+        SupportMapFragment mMapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.mapResult);
-        mapFragment.getMapAsync(this);
+        mMapFragment.getMapAsync(this);
     }
 
     @Override
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
         // Refer to the reference doc for ConnectionResult to see what error codes might
         // be returned in onConnectionFailed.
-        Log.d(TAG, "Play services connection failed: ConnectionResult.getErrorCode() = "
+        Log.e(TAG, "Play services connection failed: ConnectionResult.getErrorCode() = "
                 + connectionResult.getErrorCode());
     }
 
     @Override
     public void onConnectionSuspended(int i) {
         Log.d(TAG, "Play services connection suspended");
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        mLocationPermissionGranted = false;
+        switch (requestCode) {
+            case PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    mLocationPermissionGranted = true;
+                }
+            }
+        }
+        updateLocationUI();
+    }
+
+    @Override
+    public void onResponse(Call<RouteApiResponse> call, Response<RouteApiResponse> response) {
+        RouteApiResponse resp = response.body();
+        String status = resp.getStatus();
+        if (!status.equals("OK")) {
+            Log.e(TAG, status);
+            Log.e(TAG, resp.getErrorMessage());
+        }
+        else {
+            List<Route> routes = resp.getRoutes();
+            Route bestRoute = routes.get(0);
+            List<Leg> legs = bestRoute.getLegs();
+            Leg leg = legs.get(0);
+            List<Step> steps = leg.getSteps();
+            mRouteStartLocation = steps.get(0).getStartLocation();
+            for (Step step : steps) {
+                mRoutePoints.add(step.getEndLocation());
+            }
+        }
+    }
+
+    @Override
+    public void onFailure(Call<RouteApiResponse> call, Throwable t) {
+        Log.e(TAG, t.getMessage());
     }
 
     @Override
@@ -147,7 +188,7 @@ public class ResultMapActivity extends AppCompatActivity implements OnMapReadyCa
                     new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
                     PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
         }
-        // A step later in the tutorial adds the code to get the device location.
+
         if (mLocationPermissionGranted) {
             mLastKnownLocation = LocationServices.FusedLocationApi
                     .getLastLocation(mGoogleApiClient);
@@ -166,31 +207,11 @@ public class ResultMapActivity extends AppCompatActivity implements OnMapReadyCa
         }
     }
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        mLocationPermissionGranted = false;
-        switch (requestCode) {
-            case PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION: {
-                // If request is cancelled, the result arrays are empty.
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    mLocationPermissionGranted = true;
-                }
-            }
-        }
-        updateLocationUI();
-    }
-
-    public void updateLocationUI(){
+    private void updateLocationUI(){
         if (mMap == null) {
             return;
         }
 
-    /*
-     * Request location permission, so that we can get the location of the
-     * device. The result of the permission request is handled by a callback,
-     * onRequestPermissionsResult.
-     */
         if (ContextCompat.checkSelfPermission(this.getApplicationContext(),
                 android.Manifest.permission.ACCESS_FINE_LOCATION)
                 == PackageManager.PERMISSION_GRANTED) {
@@ -210,71 +231,46 @@ public class ResultMapActivity extends AppCompatActivity implements OnMapReadyCa
         }
     }
 
-    public void addMarkersAndRoute(){
+    private void addMarkersAndRoute(){
         if (mMap == null) {
             return;
         }
 
+        mMap.clear();
+
         if (mLastKnownLocation!=null) {
             mLatLngLastKnown = new LatLng(mLastKnownLocation.getLatitude(),
                     mLastKnownLocation.getLongitude());
-            mCameraPosition = new CameraPosition.Builder().target(mLatLngLastKnown).zoom(13.0f).build();
         }
 
         LatLngBounds.Builder builder = new LatLngBounds.Builder();
 
-        if (mLastKnownLocation!=null)
+        if (mLastKnownLocation!=null) {
             builder.include(mLatLngLastKnown);
+            mMap.addMarker(new MarkerOptions().position(mLatLngLastKnown).title("Current location"));
+        }
         builder.include(mLatLngFrom);
         builder.include(mLatLngTo);
 
         LatLngBounds bounds = builder.build();
 
+        mMap.addMarker(new MarkerOptions().position(mLatLngFrom).title("Position From"));
+        mMap.addMarker(new MarkerOptions().position(mLatLngTo).title("Position To"));
+
         PolylineOptions polylineOptions = new PolylineOptions();
-        if (routeStartLocation!=null&routePoints!=null){
-            polylineOptions.add(new LatLng(routeStartLocation.getLat(),routeStartLocation.getLng()));
-            for (EndLocation point:routePoints) {
+        if (mRouteStartLocation !=null& mRoutePoints !=null){
+            polylineOptions.add(new LatLng(mRouteStartLocation.getLat(), mRouteStartLocation.getLng()));
+            for (EndLocation point: mRoutePoints) {
                 polylineOptions.add(new LatLng(point.getLat(),point.getLng()));
             }
         }
 
-        polylineOptions.width(5);
-        polylineOptions.color(Color.BLACK);
+        polylineOptions.width(10);
+        polylineOptions.color(Color.RED);
         Polyline polyline = mMap.addPolyline(polylineOptions);
 
         CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngBounds(bounds, 100);
         mMap.animateCamera(cameraUpdate);
     }
 
-    @Override
-    public void onResponse(Call<RouteApiResponse> call, Response<RouteApiResponse> response) {
-        RouteApiResponse resp = response.body();
-        String status = resp.getStatus();
-        if (!status.equals("OK")) {
-            Log.e(TAG, status);
-            Log.e(TAG, resp.getErrorMessage());
-        }
-        else {
-            Log.d(TAG, "It work! Status = " + status);
-            List<Route> routes = resp.getRoutes();
-            Log.d(TAG, "routes.size = " + routes.size());
-            Route bestRoute = routes.get(0);
-            List<Leg> legs = bestRoute.getLegs();
-            Log.d(TAG, "legs.size = " + legs.size());
-            Leg leg = legs.get(0);
-            List<Step> steps = leg.getSteps();
-            Log.d(TAG, "steps.size = " + steps.size());
-            routeStartLocation = steps.get(0).getStartLocation();
-            Log.d(TAG, "StartLocationLatLng = " + routeStartLocation.getLat()+","+routeStartLocation.getLng());
-            for (Step step : steps) {
-                routePoints.add(step.getEndLocation());
-                Log.d(TAG, "RoutePointLatLng = " + step.getEndLocation().getLat()+","+step.getEndLocation().getLng());
-            }
-        }
-    }
-
-    @Override
-    public void onFailure(Call<RouteApiResponse> call, Throwable t) {
-        Log.e(TAG, t.getMessage());
-    }
 }
